3931cc1394bf0bdd0ac4607a40b4504fff8d3f41
muskan <vmuskan709@gmail.com>
2026-06-29 11:39:28 +0530

    NEW

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const User=require("../models/User");
+const jwt=require("jsonwebtoken");
+const bcryptjs=require("bcryptjs");
+
+const register=()=>{
+    try{
+
+    }
+    catch(err){
+        res.status(500)>json({
+            success:false,
+            message:"unable to register",
+            error:err.message
+        })
+    }
+
+};
+const login=()=>{
+
+};
+const profile=()=>{
+
+};
+const logout=()=>{
+
+};
+module.exports=
