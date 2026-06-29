34d3d6c327bdd500864aaeaf03762eb02c7e14f9
muskan <vmuskan709@gmail.com>
2026-06-29 11:05:36 +0530

    new

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt =require("jsonwebtoken");
+const User=require("../models/User");
+const authMiddleware =async(req,res,next)=>{
+    try{
+const authHeader=req.headers.authorization;
+if(!authHeader || authHeader.startsWith("Bearer")){
+    return res.status(401).json({
+        success:false,
+        message:"token not found"
+    });
+}
+let token=authHeader.split(" ")[1];
+const decoded =jwt.verify(token,process.env.SECRET_KEY);
+const user=await User.findById({id:decoded.id});
+if(!user){
+    return res.status(401).json({
+        success:false,
+        message:"user not found"
+    });
+}
+console.log("Middleware wala user",user);
+req.user=user;
+next();
+
+    }
+    catch(err){
+console.log("some error in authentication",err);
+return res.json({
+    success:false,
+    message:"invalid token"
+});
+    }
+}
